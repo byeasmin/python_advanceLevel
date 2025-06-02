9d9fbfeb49b1e91847ade2dda96d1d49ed2f38bc
Benin Yeasmin <beninyeasmin@gmail.com>
2025-06-02 08:46:43 +0600

    added all folders

---
+++ enumerate/demo_1.py
@@ -0,0 +1,58 @@
+for char in enumerate("helllow"):
+    print(char)
+
+
+for i,char in enumerate("python"):
+    print(i,char)
+
+
+for j in enumerate("programmar"):
+    print(j)
+
+
+for i, char in enumerate([1,2,3]):
+    print(i,char)
+
+
+for i, char in enumerate((2,3,4)):
+    print(i,char)
+
+
+
+
+
+
+
+
+'''
+Output : 
+(0, 'h')
+(1, 'e')
+(2, 'l')
+(3, 'l')
+(4, 'l')
+(5, 'o')
+(6, 'w')
+0 p
+1 y
+2 t
+3 h
+4 o
+5 n
+(0, 'p')
+(1, 'r')
+(2, 'o')
+(3, 'g')
+(4, 'r')
+(5, 'a')
+(6, 'm')
+(7, 'm')
+(8, 'a')
+(9, 'r')
+0 1
+1 2
+2 3
+0 2
+1 3
+2 4
+'''
---
+++ enumerate/demo_2.py
@@ -0,0 +1,8 @@
+for i, char in enumerate(list(range(100))):
+    print(i,char)
+
+#if char equal to 50 print the value of i... because the value of i and char starts at 0...
+
+    if char==50:
+        print(f'index of 50 is: {i}')
+        # print("index of 50 is : ",i)
