540bef53f7b44016822eff5870499d10ca451b54
Benin Yeasmin <beninyeasmin@gmail.com>
2025-06-03 21:14:15 +0600

    added all folders

---
+++ loop/while_loop/demo_1.py
@@ -0,0 +1,32 @@
+my_list=[2,4,6]
+
+for item in my_list:
+    print(item)
+
+
+i=0
+while i<len(my_list):
+    #print(i)
+    print(my_list[i])
+    print(my_list)
+    i+=1
+
+
+
+
+
+
+
+'''
+Output : 
+2
+4
+6
+2
+[2, 4, 6]
+4
+[2, 4, 6]
+6
+[2, 4, 6]
+'''
+
---
+++ loop/while_loop/demo_2.py
@@ -0,0 +1,23 @@
+while True:
+    response = input("say something : ")
+    if response=='bye':
+        break
+
+
+
+
+
+
+
+
+
+
+'''
+Output : 
+say something : hey
+say something : hellow
+say something : how're you?
+say something : i'm fine
+say something : bye
+
+'''
