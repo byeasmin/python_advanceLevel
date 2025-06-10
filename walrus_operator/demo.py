2a6291cc1bc3874afdd72ef9ebfdd4466e8c227f
Benin Yeasmin <beninyeasmin@gmail.com>
2025-06-10 10:32:02 +0600

    added files & folders

---
+++ walrus_operator/demo.py
@@ -0,0 +1,27 @@
+a='hellooooooooooooooo'
+if (len(a) > 10):
+    print(f'too long {len(a)} element')
+
+
+
+# warlus operator
+a = 'hellooooooooooooooo'
+if ((n := len(a)) > 10):
+    print(f'too long {n} element')
+
+
+
+
+'''
+Output : 
+too long 19 element
+too long 19 element
+
+'''
+
+
+
+
+
+
+
---
+++ walrus_operator/demo_1.py
@@ -0,0 +1,39 @@
+a = 'hellooooooooooooooo'
+while ((n := len(a)) > 1):
+    print(n)
+    a = a[:-1]
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Output : 
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+h
+'''
